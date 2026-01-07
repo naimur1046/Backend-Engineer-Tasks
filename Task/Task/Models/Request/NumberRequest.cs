48d4d70d44899f72dd43046b1bc3d83b5a9e282b
Naimur Rahman <naimur.onnorkomo@gmail.com>
2026-01-07 17:53:31 +0600

    update validation in the number request

--- Task/Task/Models/Request/NumberRequest.cs
+++ Task/Task/Models/Request/NumberRequest.cs
@@ -2,8 +2,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Controllers.Models.Request;
 
-public class NumberRequest
+public class NumberRequest : IValidatableObject
 {
     [Required]
-    public int Number { get; set; }
+    [Range(0, 999.99, ErrorMessage = "Number must be between 0 and 999.99")]
+    public decimal Number { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var decimalPlaces = BitConverter.GetBytes(decimal.GetBits(Number)[3])[2];
+        if (decimalPlaces > 2)
+        {
+            yield return new ValidationResult(
+                "Number can have at most 2 decimal places",
+                new[] { nameof(Number) });
+        }
+    }
 }
