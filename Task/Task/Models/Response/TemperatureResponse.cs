76db82c06a40b18adc71c6d4b1e23f2940efc143
naimur1046 <naimurrahman1046@gmail.com>
2026-01-08 20:01:39 +0600

    update temperature response after adding json property

--- Task/Task/Models/Response/TemperatureResponse.cs
+++ Task/Task/Models/Response/TemperatureResponse.cs
@@ -1,11 +1,18 @@
+using System.Text.Json.Serialization;
 namespace Controllers.Models.Response;
 
 public class TemperatureResponse
 {
-    public double Min  { get; set; } 
+    [JsonPropertyName("min")]
+    public double Min  { get; set; }
+    [JsonPropertyName("max")]
     public double Max { get; set; }
+    [JsonPropertyName("average")]
     public double Average { get; set; }
+    [JsonPropertyName("minText")]
     public string MinText { get; set; } = string.Empty;
+    [JsonPropertyName("maxText")]
     public string MaxText { get; set; } = string.Empty;
+    [JsonPropertyName("averageText")]
     public string AverageText { get; set; } = string.Empty;
 }
